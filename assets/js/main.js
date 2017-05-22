d5730a86516b3df8001b7ff8dbf9719b24fdbe79
berenisser <berenisser@gmail.com>
2017-05-21 20:57:30 -0400

    Toggle buttons for text ready

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+document.getElementById("origin-izq").addEventListener("click",function(){
+	var letraIz = document.getElementById("left");
+	letraIz.classList.toggle("display-nada");
+})
+
+document.getElementById("extincion-derecha").addEventListener("click",function(){
+	var letraDer = document.getElementById("right");
+	letraDer.classList.toggle("display-nada");
+})
